3613bb1730a9cfa4582be828d156efdc7e07c58a
Ian Hudis <52722993+Ian-Hudis@users.noreply.github.com>
2026-01-22 10:06:32 -0600

    Add files via upload

---
+++ BlackboxServer/obj/Release/net8.0/PubTmp/Out/wwwroot/TCP_Server.cs
@@ -0,0 +1,30 @@
+﻿namespace BlackboxServer.wwwroot
+{
+    public class TCP_Server
+    {
+
+        public struct PLC_Data
+        {
+            public PLCrawInput RAWdata;
+
+            public string Supervisor;
+            public string Operator;
+            public string ProductionOrder;
+            public string Material;
+            public string Sevent;
+            public string Status; // Enable/Disable status
+            public string Override;
+
+            public string prevSupervisor;
+            public string prevOperator;
+            public string prevProductionOrder;
+            public string prevMaterial;
+            public string prevSevent;
+            public string prevStatus;
+            public string prevOverride;
+
+            public bool DetectChange;
+        }
+
+    }
+}
